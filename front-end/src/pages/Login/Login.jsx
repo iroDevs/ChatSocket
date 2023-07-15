733d0e8637f98d83d81f19e78c61cf165f98b95f
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-07-15 20:21:46 -0300

    Front end & back-end struct

---
+++ front-end/src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Login = () => {
+    return (
+        <>
+          <h1>Login</h1>  
+        </>
+    );
+}
+
+export default Login;
---
+++ front-end/src/pages/WebChat/WebChat.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const WebChat = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default WebChat;
